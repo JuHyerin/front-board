96e5e49441edf60c0a01d5dc7ebc6161386f4aa7
JuHyerin <jhr67370719@gmail.com>
2020-12-16 19:09:54 +0900

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,10 +15,7 @@ export default {
   },
   methods: {
     getPosts: function () {
-      axios
-        .get('http://localhost:8080/hi', {
-          headers: {'Authorization' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNjkyMDc1QGhhbnN1bmcuYWMua3IiLCJleHAiOjE2MDgxMTAyMTEsImlhdCI6MTYwODEwNjYxMSwidXNlciI6eyJ1c2VybmFtZSI6IjE2OTIwNzVAaGFuc3VuZy5hYy5rciIsIm5hbWUiOiIo7Lu07ZOo7YSw6rO17ZWZ67aAKeyjvO2YnOumsCIsInBpY3R1cmUiOiJodHRwczpcL1wvbGg1Lmdvb2dsZXVzZXJjb250ZW50LmNvbVwvLTZJQk43QmpfZXBBXC9BQUFBQUFBQUFBSVwvQUFBQUFBQUFBQUFcL0FNWnV1Y25OS3VQZzZkYy01SnVQaEF2NDBDV1NUQ1pBSkFcL3M5Ni1jXC9waG90by5qcGciLCJyb2xlcyI6WyJST0xFX01FTUJFUiJdfX0.SsqVeeobWnwSx2MaAY9ARvB71Z2qTJOjKtPN1J6STPg'}
-          })
+      axios.get('http://localhost:8080/hi', { headers: {'Authorization' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNjkyMDc1QGhhbnN1bmcuYWMua3IiLCJleHAiOjE2MDgxMTAyMTEsImlhdCI6MTYwODEwNjYxMSwidXNlciI6eyJ1c2VybmFtZSI6IjE2OTIwNzVAaGFuc3VuZy5hYy5rciIsIm5hbWUiOiIo7Lu07ZOo7YSw6rO17ZWZ67aAKeyjvO2YnOumsCIsInBpY3R1cmUiOiJodHRwczpcL1wvbGg1Lmdvb2dsZXVzZXJjb250ZW50LmNvbVwvLTZJQk43QmpfZXBBXC9BQUFBQUFBQUFBSVwvQUFBQUFBQUFBQUFcL0FNWnV1Y25OS3VQZzZkYy01SnVQaEF2NDBDV1NUQ1pBSkFcL3M5Ni1jXC9waG90by5qcGciLCJyb2xlcyI6WyJST0xFX01FTUJFUiJdfX0.SsqVeeobWnwSx2MaAY9ARvB71Z2qTJOjKtPN1J6STPg'}})
         .then((result) => {
           console.log(result);
           this.name = result.data.contents;
